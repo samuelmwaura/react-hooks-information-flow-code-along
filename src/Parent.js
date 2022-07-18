f4779e9ff31be9ed60eccadc72ab42360deb3e3b
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-07-18 14:48:40 +0300

    completed the code along

--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,20 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [parentColor, setParentColor] = useState(randomColor);
+  const [childColor,setChildColor] = useState('#FFF');
+
+ function onChildClick(newChildColor){
+  const randomColor = getRandomColor();
+  setParentColor(randomColor)
+  setChildColor(newChildColor)
+ }
+
 
   return (
-    <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+    <div className="parent" style={{backgroundColor: parentColor}}>
+      <Child color= {childColor} onChangeColor={onChildClick} /> {/*since set color receives a callback function*/}
+      <Child color={childColor} onChangeColor={onChildClick} />  {/* Instead of defining another function, we just flow with the setState function that we already know.*/}
     </div>
   );
 }
